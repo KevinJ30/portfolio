efe55a562abb8807d11fd8c5cd995cf16f772699
Joudrier Kevin <kev.joudrier@gmail.com>
2021-07-22 06:29:34 +0200

    Modification de l'icon du post type skill.

--- src/PostType/AbstractPostType.php
+++ src/PostType/AbstractPostType.php
@@ -20,11 +20,23 @@ abstract class AbstractPostType
      **/
     protected string $menuName;
 
-    public function __construct(string $name, string $singularName, string $menuName)
+    /**
+     * @var string $menuIcon Icon in the menu
+     **/
+    protected string $menuIcon;
+
+    /**
+     * @var array $supports All the data supports for the post type
+     **/
+    protected array $supports;
+
+    public function __construct(string $name, string $singularName, string $menuName, string $menuIcon = "dashicons-admin-post", array $supports = ['title', 'excerpt', 'author'])
     {
         $this->name = $name;
         $this->singularName = $singularName;
         $this->menuName = $menuName;
+        $this->menuIcon = $menuIcon;
+        $this->supports = $supports;
 
         add_action('init', [$this, 'register']);
     }
@@ -34,23 +46,41 @@ abstract class AbstractPostType
         register_post_type('news', $this->arguments());
     }
 
-    public function label() : array
+    protected function label() : array
     {
         return [
             'name'                => _x($this->name, 'Post Type General Name'),
             'singular_name'       => _x($this->singularName, 'Post Type Singular Name'),
             'menu_name'           => __($this->name),
-            'all_items'           => __('Toutes les' . $this->name),
-            'view_item'           => __('Voir les' . $this->name),
-            'add_new_item'        => __('Ajouter une nouvelle' . $this->singularName),
+            'all_items'           => __('Toutes les ' . $this->name),
+            'view_item'           => __('Voir les ' . $this->name),
+            'add_new_item'        => __('Ajouter une nouvelle ' . $this->singularName),
             'add_new'             => __('Ajouter'),
-            'edit_item'           => __('Editer la' . $this->singularName),
-            'update_item'         => __('Modifier la' . $this->singularName),
+            'edit_item'           => __('Editer la ' . $this->singularName),
+            'update_item'         => __('Modifier la ' . $this->singularName),
             'search_items'        => __('Rechercher une' . $this->singularName),
             'not_found'           => __('Non trouvée'),
             'not_found_in_trash'  => __('Non trouvée dans la corbeille'),
         ];
     }
 
-    abstract protected function arguments();
+    protected function arguments() : array
+    {
+        return [
+            'label'               => __($this->name),
+            'description'         => __('Tous sur la ' . $this->name),
+            'labels'              => $this->label(),
+            // On définit les options disponibles dans l'éditeur de notre custom post type ( un titre, un auteur...)
+            'supports'            => $this->supports,
+            /*
+            * Différentes options supplémentaires
+            */
+            'show_in_rest' => true,
+            'hierarchical'        => false,
+            'public'              => true,
+            'has_archive'         => true,
+            'rewrite'             => array( 'slug' => $this->name),
+            'menu_icon'           => $this->menuIcon
+        ];
+    }
 }
--- web/app/themes/kevinj30_portfolio/classes/Domain/Skills/PostType/SkillPostType.php
+++ web/app/themes/kevinj30_portfolio/classes/Domain/Skills/PostType/SkillPostType.php
@@ -7,25 +7,6 @@ class SkillPostType extends AbstractPostType
 {
     public function __construct()
     {
-        parent::__construct('Compétences', 'Compétence', 'Compétences');
-    }
-
-    protected function arguments() : array
-    {
-        return [
-            'label'               => __($this->name),
-            'description'         => __('Tous sur la' . $this->name),
-            'labels'              => $this->label(),
-            // On définit les options disponibles dans l'éditeur de notre custom post type ( un titre, un auteur...)
-            'supports'            => array( 'title', 'excerpt', 'author'),
-            /*
-            * Différentes options supplémentaires
-            */
-            'show_in_rest' => true,
-            'hierarchical'        => false,
-            'public'              => true,
-            'has_archive'         => true,
-            'rewrite'             => array( 'slug' => $this->name),
-        ];
+        parent::__construct('Compétences', 'Compétence', 'Compétences', 'dashicons-awards');
     }
 }
