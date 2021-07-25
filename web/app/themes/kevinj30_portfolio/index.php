642df89882d5705caf42a571c569e5cbe1fa2e4a
Joudrier Kevin <kev.joudrier@gmail.com>
2021-07-25 16:13:26 +0200

    Feature home 2 (#3)

--- web/app/themes/kevinj30_portfolio/index.php
+++ web/app/themes/kevinj30_portfolio/index.php
@@ -1,18 +1,1 @@
-<?php
 
-use Timber\Timber;
-
-$context = Timber::get_context();
-$context['title'] = get_theme_mod('KevinJ30_setting_introduction_title');
-$context['introduction'] = get_theme_mod('KevinJ30_setting_introduction_text');
-$context['introduction_image'] = get_theme_mod('KevinJ30_setting_introduction_svg');
-
-$context['skills'] = Timber::get_posts([
-    'post_type' => 'compétence'
-]);
-
-$context['projects'] = Timber::get_posts([
-   'post_type' => 'Projet'
-]);
-
-Timber::render('pages/home/index.twig', $context);
